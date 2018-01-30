b8321ecca40642c8fc6a69901ccf1bbd9fb71985
TheTash <t.kashi.92@gmail.com>
2018-01-30 22:38:29 +0000

    func countsdown the characters, and goes red after passing 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
-  $("#tweet").keydown(function() {
-    console.log("test");
+$(document).ready(function () {
+  $("#tweet").on('keyup', function () {
+    const tweetMax = 140;
+    let typedInput = $('#tweet')[0].value;
+    let charRemaining = tweetMax - typedInput.length;
+
+    if (charRemaining > 0){
+      $(this).siblings(".counter").html(charRemaining);
+    } else {
+      $(this).siblings(".counter").html(charRemaining)
+      .css("color", "red");
+    }
   });
 });
 
-document.addEventListener("dblclick", function (event) {
-  console.log(event);
-});
+
+
+//2
+//
