832bd3ff8b059a52a628c63acfde5c4a65fe8e78
TheTash <t.kashi.92@gmail.com>
2018-01-30 19:03:24 +0000

    starting jquery stuff, keydowns are registering in console

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#tweet").keydown(function() {
+    console.log("test");
+  });
+});
+
+document.addEventListener("dblclick", function (event) {
+  console.log(event);
+});
