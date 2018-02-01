171544bd7a8f364abd32e6f6e50900b097ccb384
TheTash <t.kashi.92@gmail.com>
2018-02-01 02:48:49 +0000

    bewary of label, will need to do extra changes to protect char-Counting function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,4 @@
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
   $(document).ready(function(){
     function createTweetElement(tweet) {
       let $article = $('<article>').addClass('tweet-box');
@@ -78,7 +31,6 @@ const data = [
       const articles_elm = $('.restOfTheTweets')
       tweets.forEach((tweet) => {
         articles_elm.prepend(createTweetElement(tweet));
-
       });
     }
       $("form").submit(function(event){
@@ -94,10 +46,4 @@ const data = [
           renderTweets(data);
         })
       });
-/*
-      function loadTweets() {
-
-        articles_elm.append(createTweetElement(tweet));
-      }
-*/
     });
