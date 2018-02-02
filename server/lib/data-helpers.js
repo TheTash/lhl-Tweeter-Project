db25edcdbbfb4d45ba88efeb8bf24b0b56dd8f18
TheTash <t.kashi.92@gmail.com>
2018-02-02 19:00:23 +0000

    mongodb conversion statrted, db has connected with site.  css chagnes to be made though.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -13,14 +13,12 @@ module.exports = function makeDataHelpers(db) {
         db.tweets.push(newTweet);
         callback(null, true);
       });
+      db.collection("tweets").insertOne()
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection("tweets").find().toArray(callback);
     }
 
   };
